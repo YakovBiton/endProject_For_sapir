413924e655bf0ebb609bcf36b7ab802c58752eb3
kobbi <kobi9991@gmail.com>
2023-03-22 13:09:27 +0200

    k

---
+++ Classifier_PyTorch.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class FacialFeaturesNet(nn.Module):
+    def __init__(self):
+        super(FacialFeaturesNet, self).__init__()
+        self.fc1 = nn.Linear(6, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 6)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
